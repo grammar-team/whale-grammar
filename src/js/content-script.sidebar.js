158f01c2bc52deff2e1476bc2c33252a07bdc24a
이동현 <dlehdanakf@konkuk.ac.kr>
2019-10-28 00:24:19 +0900

    항상 "검사하기" 버튼이 노출되도록 버튼 clone

--- src/js/content-script.sidebar.js
+++ src/js/content-script.sidebar.js
@@ -1,4 +1,36 @@
-/**
- *	@Author 이동현, 한승진
- */
-console.log(`Hello World!`);
+function isSidebar() {
+	const { dataset } = document.documentElement;
+	const { userAgent } = window.navigator;
+	const { href } = window.location;
+	if(
+		userAgent.includes(`sidebar`) &&
+		dataset.useragent.includes(`sidebar`) &&
+		href.includes(`whale-grammar`)
+	) {
+		return true;
+	}
+
+	return false;
+}
+function duplicateInspectButton(parentEl) {
+	const buttonEl = parentEl.querySelector(`button`);
+	const { innerText, className } = buttonEl;
+
+	const nodeEl = document.createElement(`span`);
+	nodeEl.innerText = innerText;
+	nodeEl.className = className;
+
+	buttonEl.parentNode.insertBefore(nodeEl, buttonEl);
+}
+
+document.addEventListener(`DOMContentLoaded`, function() {
+	if(isSidebar() === false) {
+		return;
+	}
+
+	const bodyEl = document.querySelector(`body`);
+	bodyEl.classList.add(`whale-grammar`);
+
+	const sectionEl = document.querySelector(`#grammar_checker`);
+	duplicateInspectButton(sectionEl);
+});
